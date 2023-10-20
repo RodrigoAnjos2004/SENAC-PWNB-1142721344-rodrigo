855365f61237a07cc932d279c2a0ee3d07748e59
Rodrigo Silva dos Anjos <89110372+RodrigoAnjos2004@users.noreply.github.com>
2023-10-20 16:59:33 -0300

    Add files via upload

---
+++ PWNB_ENTREGAS/PWNB-Semana#05/script.js
@@ -0,0 +1,16 @@
+
+// function abrirBarraLateral() {
+//     document.getElementById("minhaBarraLateral").style.transform = "translateX(0px)";
+//     document.getElementById("principal").style.marginLeft = "250px";
+// }
+
+// function fecharBarraLateral() {
+//     document.getElementById("minhaBarraLateral").style.transform = "translateX(-250px)";
+//     document.getElementById("principal").style.marginLeft = "0";
+// }
+
+// var carrossel;
+// $(document).ready(function () {
+//     carrossel = $("#rolagem ul");
+//     carrossel.itemslide();
+// });
